2469ae21b49de3db3ebc010cc1ef165f23bb5a64
Blake Smith <besmith43@gmail.com>
2021-04-10 17:55:48 -0500

    saving initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Security.Principal;
+
+namespace TTU_DefaultFileAssociations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (IsAdministrator())
+            {
+                if (File.Exists(@"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.exe"))
+                {
+                    // run dism for mailto
+                }
+
+                if (File.Exists(@"C:\Program Files\ImageGlass\ImageGlass.exe"))
+                {
+                    // run dism for imageglass
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please Run as Admin");
+            }
+        }
+
+        public static bool IsAdministrator()
+        {
+            return (new WindowsPrincipal(WindowsIdentity.GetCurrent()))
+                 .IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
